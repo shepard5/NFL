2881c0ef6aa8ced5d93ce6164b8544cd3125d36d
shepard5 <108085853+shepard5@users.noreply.github.com>
2023-08-18 02:02:38 -0400

    Add files via upload
    Consolidated functions to one, removed unused/redundant code, switched to utilizing dictionaries over lists when storing data within each function, minimized excel export process

---
+++ NFL_Data_Function.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import requests
+import openpyxl
+
+def NFL_Data():
+
+#ATS Streak
+    
+    dict_ats = {i:0 for i in range (1,35)} 
+
+    for x in range(1, 35):
+        team_url = f'https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/teams/{x}/odds/1002/past-performances?limit=300'
+        response = requests.get(team_url)
+        ats_historical = response.json()
+        game_count = len(ats_historical['items'])
+
+        if game_count > 1:
+            week_recent = ats_historical["items"][game_count-1]
+            week_2nd_recent = ats_historical["items"][game_count-2]
+
+            if week_recent["spreadWinner"] == week_2nd_recent["spreadWinner"]: 
+                if week_recent["spreadWinner"] == True:
+                    prospect = 'hot'
+                else:
+                    prospect = 'cold'
+            
+            dict_ats[x] = prospect
+    
+
+#QBR
+            
+    #Search for most recent QBR data
+    for x in range(23,21,-1):
+        for y in range(18,0,-1):
+            url = f"https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/20{x}/types/2/weeks/{y}/qbr/10000?limit=300"       
+            headers = {
+    
+            }
+            response = requests.get(url, headers=headers)
+            qbr_data = response.json()
+                   
+            if len(qbr_data["items"])>1:
+                year = f"20{x}"
+                week = y
+                break       
+
+    #Store QBR data to corresponding dictionary key (teamID)
+    dict_qbr = {i:0 for i in range(1,35)}
+    seen = set()
+    
+    for x in range(0,len(qbr_data["items"])-1): 
+
+        team_qbr = qbr_data["items"][x]["splits"]["categories"][0]["stats"][17]["value"]
+        team_id_url = qbr_data["items"][x]["team"]["$ref"]
+
+        if team_id_url[83] != "?":
+            team_id = int(team_id_url[82] + team_id_url[83])
+        else:
+            team_id = int(team_id_url[82])
+
+        if team_id not in seen and x<35 :
+            seen.add(team_id)
+            dict_qbr[team_id] = team_qbr            
+#Export
+            
+    excel_data = {i:(dict_ats[i],dict_qbr[i]) for i in range(1,35)}
+    
+    df = pd.DataFrame(list(excel_data.items()), columns=['Team ID', 'Data'])
+    df[['ATS Streak', 'QBR']] = pd.DataFrame(df['Data'].tolist(), index=df.index)
+    df.drop('Data', axis=1, inplace=True)
+    df.to_excel("NFL_Data.xlsx", index=False, engine='openpyxl')

51e31187f14dbcb84f7d59729d25452eb79b63ab
shepard5 <108085853+shepard5@users.noreply.github.com>
2023-08-18 03:19:20 -0400

    Add files via upload

---
+++ NFL_RStudio.R
@@ -0,0 +1,13 @@
+data<-read_excel("C:\\Users\\Sam\\Desktop\\NFL-main\\Functions\\NFL_Data.xlsx")
+
+data_clean<-data[-c(27,28,31,32), ]
+
+y<-data_clean$`ATS Streak`
+x<-data_clean$QBR
+x_log<-log(data_clean$QBR)
+
+
+
+model<-lm(x~y, data=data_clean)
+
+summary(model)

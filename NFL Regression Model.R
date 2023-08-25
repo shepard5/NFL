10877fcfe9f7ad670b38eddc3e572c9c2ca4801a
shepard5 <108085853+shepard5@users.noreply.github.com>
2023-08-25 01:16:43 -0400

    Add files via upload

--- NFL Regression Model.R
+++ NFL Regression Model.R
@@ -68,18 +68,34 @@ QB_Heavy_IDs<-c(2,4,30,12,21,3,22,31,7,15,6,7)
 QB_Light_Wins<-Wins_2022_df$`0`[QB_Light_IDs] #Avg 8.5 Wins per team
 QB_Heavy_Wins<-Wins_2022_df$`0`[QB_Heavy_IDs] #Avg 9.33 Wins per team
 
-qb_light_model<-lm(QB_Light_Wins~data2022$`Quarterback Rating`[QB_Light_IDs]+data2022$Sacks[QB_Light_IDs])
-qb_heavy_model<-lm(QB_Heavy_Wins~data2022$`Quarterback Rating`[QB_Heavy_IDs]+data2022$Sacks[QB_Heavy_IDs])
+#Tinkering with different variables
+
+qb_light_model<-lm(QB_Light_Wins~data2022$`Quarterback Rating`[QB_Light_IDs]+data2022$Sacks[QB_Light_IDs]+data2022$`Turnover Ratio`[QB_Light_IDs]+data2022$`Possession Time Seconds`[QB_Light_IDs])
+qb_heavy_model<-lm(QB_Heavy_Wins~data2022$`Quarterback Rating`[QB_Heavy_IDs]+data2022$Sacks[QB_Heavy_IDs]+data2022$`Turnover Ratio`[QB_Heavy_IDs]+data2022$`Possession Time Seconds`[QB_Heavy_IDs])
+
+#Checking for multicollinearity
+combined_df_heavy<-data.frame(data2022$`Quarterback Rating`[QB_Heavy_IDs],data2022$Sacks[QB_Heavy_IDs],data2022$`Turnover Ratio`[QB_Heavy_IDs],data2022$`Possession Time Seconds`[QB_Heavy_IDs])
+combined_df_light<-data.frame(data2022$`Quarterback Rating`[QB_Light_IDs],data2022$Sacks[QB_Light_IDs],data2022$`Turnover Ratio`[QB_Light_IDs],data2022$`Possession Time Seconds`[QB_Light_IDs])
+
+cor_matrix<-cor(combined_df,use = "complete.obs", method = "pearson")
+
+##Collinearity doesn't seem to be a major issue with these variables - all at or below 0.5. What is concerning is for the QB light data set, QBR and defensive sacks appear to be
+#correlated far higher than expectations.
+
+#Will have to make sure QB light and heavy teams analysis isn't also a QB bad and good analysis. Maybe will have to look at passing attempts 
+#and/or number of plays with meaningful/beyond-expectation contributions by quarterbacks 
 
 summary(qb_light_model)
 summary(qb_heavy_model)
 
 
-#Running model using Miami Dolphins Data - Using ESPN Quarterback Rating, Expected total wins is 10.68 assuming all 
+#Running model using Miami Dolphins Data - Using ESPN Quarterback Rating
+
+
+
+#Expected total wins is 10.68 assuming all 
 #2022 conditions remains the same. Using Quarterback Rating, 10.38. Might have to look into Miami - looking like they
-#could break 9.5 wins.
-
-
+#could break 9.5 wins. Update: Jalen Ramsey is out - look into importing injury data. 
 
 
 summary(model_2022)
@@ -88,11 +104,9 @@ summary(model_2019)
 summary(model_2018)
 
 
-
-
 #8/24 Logistic regression of match-ups among teams with high predictability
 
-
+#High Predictability Candidates: 49ers,DAL,MIA,MIN,NYG,PHI,SEA,PIT
 
 
 
@@ -111,7 +125,7 @@ summary(model)
 ## Plotting data with regression line
 
 ggplot(combined_data, aes(x=combined_data$`ESPN Quarterback Rating`, y=combined_data$`Red Zone Efficiency Percentage`)) +
-  geom_point(aes(color=as.factor(year)), alpha=0.6) +  # Scatter plot with different colors for each year
+  geom_point(aes(color=as.factor(year)), alpha=0.6) +  # Different colors for each year
   geom_abline(intercept = coef(model)[1], slope = coef(model)[2], color="black", size=1.2) +
   labs(title="Scatter plot with Regression Line", x="X-axis label", y="Y-axis label") +
   theme_minimal() 

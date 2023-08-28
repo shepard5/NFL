2e007465b386697051674989a522d44940829669
shepard5 <108085853+shepard5@users.noreply.github.com>
2023-08-28 17:49:56 -0400

    Add files via upload
    Added functionality to view any week from any year

--- Sportsbook_Line_Generator.py
+++ Sportsbook_Line_Generator.py
@@ -2,16 +2,16 @@ import pandas as pd
 import requests as r
 import openpyxl
 
-def Get_Weekly_Matchups():
-    current_week_url = "https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/events"
-    response = r.get(current_week_url)
+def Get_Weekly_Matchups(year,week):
+    url_1 = "https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/" + f"{year}" + "/types/2/weeks/" + f"{week}" + "/events?lang=en%C2%AEion=us"
+    response = r.get(url_1)
     matchups = response.json()
-    dict_all_odds = {}
+    dict_schedules = {}
     dict_spreads = {}
     dict_over_under = {}
     dict_moneyline = {}
     for x in range(len(matchups["items"])):
-
+        print(x)
         #Matchup Details Request
     
         url = matchups["items"][x]["$ref"]
@@ -20,8 +20,8 @@ def Get_Weekly_Matchups():
         ind_matchup = matchup_response.json()
         matchup_id = ind_matchup["id"]
         matchup_name = ind_matchup["shortName"]
-        if matchup_name not in dict_all_odds:
-            dict_all_odds[matchup_name] = {}
+        if matchup_name not in dict_schedules:
+            dict_schedules[matchup_name] = {}
             
         #Odds Request
 
@@ -31,20 +31,27 @@ def Get_Weekly_Matchups():
     
         spreads = [odds_json["items"][x]["spread"] for x in range(len(odds_json["items"])) if "spread" in odds_json["items"][x]]
         over_under = [odds_json["items"][x]["overUnder"] for x in range(len(odds_json["items"])) if "overUnder" in odds_json["items"][x]]
-        moneyline = [[odds_json["items"][x]["awayTeamOdds"]["moneyLine"],odds_json["items"][x]["homeTeamOdds"]["moneyLine"]] for x in range(len(odds_json["items"])) if "awayTeamOdds" and "homeTeamOdds" in odds_json["items"][x]]
+        moneyline = [
+            [
+                odds_json["items"][x]["awayTeamOdds"].get("moneyLine", None),
+                odds_json["items"][x]["homeTeamOdds"].get("moneyLine", None)
+            ]
+            for x in range(len(odds_json["items"]))
+            if "awayTeamOdds" and "homeTeamOdds" in odds_json["items"][x]
+        ]
 
         #Initialize dictionary containing all stats
-        dict_all_odds[matchup_name]["Spreads"] = spreads
-        dict_all_odds[matchup_name]["MoneyLine"] = moneyline
-        dict_all_odds[matchup_name]["OverUnder"] = over_under
+        dict_schedules[matchup_name]["Spreads"] = spreads
+        dict_schedules[matchup_name]["MoneyLine"] = moneyline
+        dict_schedules[matchup_name]["OverUnder"] = over_under
 
-    for item in dict_all_odds:
+    for item in dict_schedules:
             
-        dict_spreads[item] = dict_all_odds[item]["Spreads"]
-        dict_over_under[item] = dict_all_odds[item]["OverUnder"]
-        dict_moneyline[item] = dict_all_odds[item]["MoneyLine"]
+        dict_spreads[item] = dict_schedules[item]["Spreads"]
+        dict_over_under[item] = dict_schedules[item]["OverUnder"]
+        dict_moneyline[item] = dict_schedules[item]["MoneyLine"]
         
-    df = pd.DataFrame.from_dict('Replace with desired dictionary here', orient='index')
+    df = pd.DataFrame.from_dict(dict_schedules, orient='index')
     df_transposed = df.transpose()
-    df_transposed.to_excel('weekly_odds.xlsx', engine='openpyxl')
+    df.to_excel('weekly_odds.xlsx', engine='openpyxl')
 
